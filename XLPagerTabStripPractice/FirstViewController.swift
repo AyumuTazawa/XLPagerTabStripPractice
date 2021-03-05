2c0a89a312668e613654378440b651827823c9f1
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-05 19:32:39 +0900

    Initial Commit

---
+++ XLPagerTabStripPractice/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  XLPagerTabStripPractice
+//
+//  Created by 田澤歩 on 2021/03/05.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
