5d583821d295062f6ccbfe2063864dc2b18fcd9e
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-05 19:44:41 +0900

    Second Commit

--- XLPagerTabStripPractice/FirstViewController.swift
+++ XLPagerTabStripPractice/FirstViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, IndicatorInfoProvider {
+    
+    //ここがボタンのタイトルに利用されます
+    var itemInfo: IndicatorInfo = "First"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +19,10 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    //必須
+        func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+            return itemInfo
+        }
 
 }
+
