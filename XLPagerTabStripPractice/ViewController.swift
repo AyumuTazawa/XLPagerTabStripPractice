cdc987c78893cf5ab7777568d3403a1276df9b79
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-05 19:31:34 +0900

    Initial Commit

---
+++ XLPagerTabStripPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XLPagerTabStripPractice
+//
+//  Created by 田澤歩 on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
