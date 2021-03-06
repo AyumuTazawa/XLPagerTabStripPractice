b3da2925295748fd0078f322c45d6d4cf4979291
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-06 20:13:16 +0900

    SideMenu追加

--- XLPagerTabStripPractice/FirstViewController.swift
+++ XLPagerTabStripPractice/FirstViewController.swift
@@ -8,27 +8,34 @@
 import UIKit
 import XLPagerTabStrip
 import AnimatedGradientView
+import SideMenu
 
 class FirstViewController: UIViewController, IndicatorInfoProvider {
     
     //ここがボタンのタイトルに利用されます
     var itemInfo: IndicatorInfo = "First"
-
+    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let gradient = AnimatedGradientView(frame: view.bounds)
         gradient.autoAnimate = false
         gradient.colorStrings = [["#3224AE", "#FF66FF"]]
         gradient.type = .axial
         view.addSubview(gradient)
         
+        //SideMenu
+        sideMenu.leftSide = true
+        sideMenu.setNavigationBarHidden(true, animated: false)
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: view)
+        
     }
     
-    //必須
-        func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
-            return itemInfo
-        }
-
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
+    }
+    
 }
 
