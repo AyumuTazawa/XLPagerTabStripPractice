ce7cb9c3bf4f75aa798a03d0bff38f3ef02a6a2d
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-07 00:09:29 +0900

    サイドメニューに項目を追加

--- XLPagerTabStripPractice/FirstViewController.swift
+++ XLPagerTabStripPractice/FirstViewController.swift
@@ -14,7 +14,7 @@ class FirstViewController: UIViewController, IndicatorInfoProvider {
     
     //ここがボタンのタイトルに利用されます
     var itemInfo: IndicatorInfo = "First"
-    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
+    private let sideMenu = SideMenuNavigationController(rootViewController: MenuListContoroller())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,3 +39,29 @@ class FirstViewController: UIViewController, IndicatorInfoProvider {
     
 }
 
+class MenuListContoroller: UITableViewController {
+    var items = ["First", "Second", "Third", "First", "Second", "Third", "First", "Second", "Third"]
+    
+    let darkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+    
+    override func viewDidLoad() {
+        tableView.backgroundColor = darkColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = UIColor.darkGray
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
