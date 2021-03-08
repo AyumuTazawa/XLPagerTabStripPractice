01bc5ab156eaf35f090117e489bf91526127e479
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-08 23:52:42 +0900

    背景にアニメーション追加

--- XLPagerTabStripPractice/ThirdViewController.swift
+++ XLPagerTabStripPractice/ThirdViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import XLPagerTabStrip
+import AnimatedGradientView
 
 class ThirdViewController: UIViewController, IndicatorInfoProvider {
 
@@ -15,8 +16,14 @@ class ThirdViewController: UIViewController, IndicatorInfoProvider {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+     
+        let animatedGradient = AnimatedGradientView(frame: view.bounds)
+        animatedGradient.direction = .upRight
+        animatedGradient.animationValues = [(colors: ["#2BC0E4", "#EAECC6"], .up, .axial),
+        (colors: ["#833ab4", "#fd1d1d", "#fcb045"], .right, .axial),
+        (colors: ["#003973", "#E5E5BE"], .down, .axial),
+        (colors: ["#1E9600", "#FFF200", "#FF0000"], .left, .axial)]
+        view.addSubview(animatedGradient)
     }
    
     //必須
