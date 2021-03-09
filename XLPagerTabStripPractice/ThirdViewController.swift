28832764c65879b46db4817cfd1cf554cd232635
TazawaAyumu <tazawaayumu@gmail.com>
2021-03-09 23:33:12 +0900

    progressHud追加

--- XLPagerTabStripPractice/ThirdViewController.swift
+++ XLPagerTabStripPractice/ThirdViewController.swift
@@ -8,27 +8,38 @@
 import UIKit
 import XLPagerTabStrip
 import AnimatedGradientView
+import NVActivityIndicatorView
 
 class ThirdViewController: UIViewController, IndicatorInfoProvider {
-
+    
     //ここがボタンのタイトルに利用されます
     var itemInfo: IndicatorInfo = "Third"
+    private var activityIndicatorView: NVActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         let animatedGradient = AnimatedGradientView(frame: view.bounds)
         animatedGradient.direction = .upRight
         animatedGradient.animationValues = [(colors: ["#2BC0E4", "#EAECC6"], .up, .axial),
-        (colors: ["#833ab4", "#fd1d1d", "#fcb045"], .right, .axial),
-        (colors: ["#003973", "#E5E5BE"], .down, .axial),
-        (colors: ["#1E9600", "#FFF200", "#FF0000"], .left, .axial)]
+                                            (colors: ["#833ab4", "#fd1d1d", "#fcb045"], .right, .axial),
+                                            (colors: ["#003973", "#E5E5BE"], .down, .axial),
+                                            (colors: ["#1E9600", "#FFF200", "#FF0000"], .left, .axial)]
         view.addSubview(animatedGradient)
+        
+        setupHud()
+        activityIndicatorView.startAnimating()
+        
     }
-   
-    //必須
+    
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return itemInfo
     }
-
+    
+    func setupHud() {
+        activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 60, height: 60), type: NVActivityIndicatorType.lineSpinFadeLoader, color: UIColor.white, padding: 0)
+        activityIndicatorView.center = self.view.center
+        view.addSubview(activityIndicatorView)
+    }
+    
 }
